8b28d128ea455799a8a9254b04dc7610aedd6aa5
Francisco Zavala <64326252+PacoZG@users.noreply.github.com>
2021-01-29 16:51:24 +0200

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{'me lleva la verga'}</Text>
+      <Text>{'this seems to be fun to work with'}</Text>
       <StatusBar style="auto" />
     </View>
   );
