f647165909fd5137f62e18b2d6fadfb6de90ef0b
Francisco Zavala <64326252+PacoZG@users.noreply.github.com>
2021-02-04 22:03:09 +0200

    Exercises Part2 of rate-repository

--- Part 2/src/components/AppBar.jsx
+++ Part 2/src/components/AppBar.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
+import { Link } from "react-router-native";
 import { View, StyleSheet, Text } from 'react-native';
-import Constants from 'expo-constants';
+import { ScrollView } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Constants.statusBarHeight,
+    padding: 12,
     backgroundColor: '#24292e',
     paddingBottom: 15,
-    // eslint-disable-next-line no-dupe-keys
-    paddingTop: 30,
+    paddingTop: 40,
+  },
+  scrollView: {
+    marginHorizontal: 0,
   },
   text: {
     color: 'white',
-    margin: 10,
+    marginRight: 10,
   }
 });
 
 const AppBarTab = () => {
-  return <View style={styles.container}>
-    <Text style={styles.text}>{'Repositories'}</Text>
-  </View>;
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView} horizontal >
+        <Link to="/" >
+          <Text style={styles.text}>{'Repositories '}</Text>
+        </Link>
+        <Link to="/signIn" >
+          <Text style={styles.text}>{'Sign in '}</Text>
+        </Link>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default AppBarTab;
